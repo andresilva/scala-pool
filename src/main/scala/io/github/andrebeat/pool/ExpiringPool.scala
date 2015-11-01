0e37ded46d06ee04a1ea608b98eee1f8e0de1b03
Andre Silva <andre.beat@gmail.com>
2015-11-01 03:57:43 +0000

    ExpiringPool: do not keep strong references to objects

--- src/main/scala/io/github/andrebeat/pool/ExpiringPool.scala
+++ src/main/scala/io/github/andrebeat/pool/ExpiringPool.scala
@@ -2,7 +2,7 @@ package io.github.andrebeat.pool
 
 import java.util.{ Timer, TimerTask }
 import java.util.concurrent.ArrayBlockingQueue
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.{ AtomicInteger, LongAdder }
 import scala.annotation.tailrec
 import scala.concurrent.duration.{ Duration, NANOSECONDS }
 
@@ -21,26 +21,41 @@ class ExpiringPool[A <: AnyRef](
 
   implicit private[this] def function2TimerTask[A](f: () => A) = new TimerTask() { def run() = f() }
 
-  final protected class ExpiringItem(r: Ref[A], val timerTask: TimerTask = () => {}) extends Item(r) {
+  final protected class ExpiringItem(
+      val id: Long,
+      r: Ref[A],
+      val timerTask: TimerTask = () => {}
+  ) extends Item(r) {
+
     def consume() = timerTask.cancel()
 
     def offerSuccess() = timer.schedule(timerTask, maxIdleTime.toMillis)
 
     override def equals(that: Any) = that match {
-      case that: ExpiringItem @unchecked => this.r eq that.r
+      case that: ExpiringItem @unchecked => this.id == that.id
       case _ => false
     }
   }
 
   private[this] val timer = new Timer(s"scala-pool-${ExpiringPool.count.getAndIncrement}", true)
 
+  // Since we're only using the `sum()` and `increment()` methods, the `LongAdder` is guaranteed to
+  // be sequentially consistent (and faster than `AtomicLong`).
+  // http://concurrencyfreaks.blogspot.se/2013/09/longadder-is-not-sequentially-consistent.html
+  private[this] val adder = new LongAdder()
+
   @inline protected[this] def factory() = _factory()
   @inline protected[this] def dispose(a: A) = _dispose(a)
   @inline protected[this] def reset(a: A) = _reset(a)
 
   @inline protected[this] def newItem(a: A) = {
+    adder.increment()
+    val id = adder.sum()
     val r = Ref(a, referenceType)
-    new ExpiringItem(r, () => if (items.remove(new ExpiringItem(r))) destroy(a))
+    new ExpiringItem(id, r, () => {
+      val i = new ExpiringItem(id, r)
+      if (items.remove(i)) i.destroy()
+    })
   }
 }
 
