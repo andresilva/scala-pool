ad96cd624aea9a5beb4c4ac6c2ae3461da003d02
Andre Silva <andre.beat@gmail.com>
2015-11-01 05:20:17 +0000

    declare Adders as final

--- src/main/java_7/io/github/andrebeat/pool/Adder.scala
+++ src/main/java_7/io/github/andrebeat/pool/Adder.scala
@@ -2,12 +2,12 @@ package io.github.andrebeat.pool
 
 import java.util.concurrent.atomic.AtomicLong
 
-private[pool] class Jdk8Adder extends Adder {
+final private[pool] class Jdk8Adder extends Adder {
   def increment() = ???
   def count() = ???
 }
 
-private[pool] class Jdk7Adder extends AtomicLong with Adder {
+final private[pool] class Jdk7Adder extends AtomicLong with Adder {
   def increment() = this.incrementAndGet
   def count() = this.get
 }
--- src/main/java_8/io/github/andrebeat/pool/Adder.scala
+++ src/main/java_8/io/github/andrebeat/pool/Adder.scala
@@ -2,11 +2,11 @@ package io.github.andrebeat.pool
 
 import java.util.concurrent.atomic.{LongAdder, AtomicLong}
 
-private[pool] class Jdk8Adder extends LongAdder with Adder {
+final private[pool] class Jdk8Adder extends LongAdder with Adder {
   def count() = this.sum
 }
 
-private[pool] class Jdk7Adder extends AtomicLong with Adder {
+final private[pool] class Jdk7Adder extends AtomicLong with Adder {
   def increment() = this.incrementAndGet
   def count() = this.get
 }
