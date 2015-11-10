5064c7d17ba4c1ebcecb07ffa581f5ee2b7362b1
Andre Silva <andre.beat@gmail.com>
2015-11-10 01:25:30 +0000

    add test application for ConcurrentBag
    Added a multi-threaded test application that performs random take and
    add operations on both a ConcurrentBag and a LinkedTransferQueue and
    afterwards compares the results (number of items in each collection).

---
+++ src/test/scala/io/github/andrebeat/pool/ConcurrentBagTest.scala
@@ -0,0 +1,67 @@
+package io.github.andrebeat.pool
+
+import java.util.concurrent.LinkedTransferQueue
+import java.util.concurrent.atomic.LongAdder
+import scala.collection.mutable
+import scala.collection.JavaConversions._
+import scala.util.Random
+
+object ConcurrencyBagTest extends App {
+  val N_THREADS = 1
+  val N_OPERATIONS = 10
+
+  case class MyObject(val x: Long)
+
+  val queue = new LinkedTransferQueue[MyObject]()
+  val bag = new ConcurrentBag[MyObject]()
+
+  val adder = new LongAdder
+
+  val takeProb = 0
+  val addProb = 1 - takeProb
+
+  def work() = {
+    if (Random.nextDouble < takeProb) {
+      queue.poll()
+      bag.tryTake()
+    } else {
+      adder.increment
+      val i = adder.sum
+
+      val myObject = new MyObject(i)
+      queue.add(myObject)
+      bag.add(myObject)
+    }
+  }
+
+  val threads =
+    for (i <- 1 to N_THREADS) yield new Thread() {
+      override def run() {
+        var i = 0
+        while (i < N_OPERATIONS) {
+          work()
+          i += 1
+        }
+      }
+    }
+
+  threads.foreach(_.start())
+  threads.foreach(_.join())
+
+  val queueSize = queue.size
+  val bagSize = {
+    var n = 0
+    var i = bag.tryTake()
+    while (i.nonEmpty) {
+      n += 1
+      i = bag.tryTake()
+      if (i.isEmpty) bag.tryTake()
+    }
+    n
+  }
+
+  println(s"queueSize: $queueSize")
+  println(s"bagSize: $bagSize")
+
+  println(s"Pass? ${queueSize == bagSize}")
+}
