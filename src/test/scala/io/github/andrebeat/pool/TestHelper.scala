e4a1125bc7084d9df3975ead77ce7cf6974edb84
Andre Silva <andre.beat@gmail.com>
2016-04-12 23:30:30 +0100

    attempt to increase build stability on travis

--- src/test/scala/io/github/andrebeat/pool/TestHelper.scala
+++ src/test/scala/io/github/andrebeat/pool/TestHelper.scala
@@ -1,12 +1,18 @@
 package io.github.andrebeat.pool
 
+import scala.annotation.tailrec
 import scala.concurrent.{ Await, Future }
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.util.Properties
 
 trait TestHelper {
   val timeDilationFactor = Properties.envOrNone("TIME_DILATION_FACTOR").map(_.toInt).getOrElse(1)
 
-  def sleep(d: Duration) = Thread.sleep((d * timeDilationFactor).toMillis)
+  @tailrec final def sleep(d: Duration): Unit =
+    if (d > 1.millis) {
+      Thread.sleep((1.millis * timeDilationFactor).toMillis)
+      Thread.`yield`()
+      sleep(d - 1.millis)
+    }
   def await[A](f: Future[A], d: Duration) = Await.result(f, d * timeDilationFactor)
 }
