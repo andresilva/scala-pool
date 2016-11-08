d015e7bb0825e7b301daa70824d076c59a8dcb42
Andre Silva <andre.beat@gmail.com>
2016-11-08 01:14:42 +0000

    disable coverage for unused Adders

--- src/main/java_7/io/github/andrebeat/pool/Adder7.scala
+++ src/main/java_7/io/github/andrebeat/pool/Adder7.scala
@@ -2,10 +2,12 @@ package io.github.andrebeat.pool
 
 import java.util.concurrent.atomic.AtomicLong
 
+// $COVERAGE-OFF$
 final private[pool] class Jdk8Adder extends Adder {
   def increment() = ???
   def count() = ???
 }
+// $COVERAGE-ON$
 
 final private[pool] class Jdk7Adder extends AtomicLong with Adder {
   def increment() = this.incrementAndGet
